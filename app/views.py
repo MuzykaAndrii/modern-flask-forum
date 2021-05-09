d38a9c857590881378397219e00bf00d75f71d72
MuzykaAndrii <andrymyzik@gmail.com>
2021-05-09 20:14:13 +0300

    created custom 404 page

--- app/views.py
+++ app/views.py
@@ -4,6 +4,11 @@ from app.models import Section, Tag, Theme, Discussion
 
 from flask_login import current_user
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html', title='Not found'), 404
+
 @app.route('/')
 def index():
     # get all sections
