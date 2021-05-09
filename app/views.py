b8c04a79b77bd89043bda1c8da000df3d20271d8
MuzykaAndrii <andrymyzik@gmail.com>
2021-05-09 21:12:14 +0300

    created layouts for themes and discussions pages, implemented some additional info in them

--- app/views.py
+++ app/views.py
@@ -19,14 +19,14 @@ def index():
     
     return render_template('main_page.html', sections=sections, tags=tags)
 
-@app.route('/blog/<section_slug>')
+@app.route('/forum/<section_slug>')
 def themes_index(section_slug):
     current_section = Section.query.filter(Section.slug==section_slug).first_or_404()
     themes = current_section.themes
 
     return render_template('themes.html', themes=themes, section_slug=current_section.slug)
 
-@app.route('/blog/<section_slug>/<theme_slug>')
+@app.route('/forum/<section_slug>/<theme_slug>')
 def discussions_index(section_slug, theme_slug):
     current_section_id = Section.query.filter(Section.slug==section_slug).first_or_404().id
     current_theme = Theme.query.filter(Theme.slug==theme_slug, Theme.section_id==current_section_id).first_or_404()
@@ -34,7 +34,7 @@ def discussions_index(section_slug, theme_slug):
     
     return render_template('discussions.html', discussions=discussions, section_slug=section_slug, theme_slug=current_theme.slug)
 
-@app.route('/blog/<section_slug>/<theme_slug>/<discussion_id>')
+@app.route('/forum/<section_slug>/<theme_slug>/<discussion_id>')
 def discussion(section_slug, theme_slug, discussion_id):
     current_section = Section.query.filter(Section.slug==section_slug).first_or_404()
     current_theme = Theme.query.filter(Theme.slug==theme_slug, Theme.section_id==current_section.id).first_or_404()
