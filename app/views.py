21b218eed75bca7fcaeafb926d2d172868166e14
MuzykaAndrii <andrymyzik@gmail.com>
2021-05-10 21:25:18 +0300

    updated titles on several pages, divided sidebar to separate template

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html', title='Not found'), 404
 
-@app.route('/')
+@app.route('/forum')
 def index():
     # get all sections
     sections = Section.query.limit(7).all()
@@ -17,14 +17,14 @@ def index():
     # get all tags
     tags = Tag.query.limit(25).all()
     
-    return render_template('main_page.html', sections=sections, tags=tags)
+    return render_template('main_page.html', sections=sections, tags=tags, title='Sections list')
 
 @app.route('/forum/<section_slug>')
 def themes_index(section_slug):
     current_section = Section.query.filter(Section.slug==section_slug).first_or_404()
     themes = current_section.themes
 
-    return render_template('themes.html', themes=themes, section_slug=current_section.slug)
+    return render_template('themes.html', themes=themes, section_slug=current_section.slug, title='Themes list')
 
 @app.route('/forum/<section_slug>/<theme_slug>')
 def discussions_index(section_slug, theme_slug):
@@ -32,7 +32,7 @@ def discussions_index(section_slug, theme_slug):
     current_theme = Theme.query.filter(Theme.slug==theme_slug, Theme.section_id==current_section_id).first_or_404()
     discussions = current_theme.discussions
     
-    return render_template('discussions.html', discussions=discussions, section_slug=section_slug, theme_slug=current_theme.slug)
+    return render_template('discussions.html', discussions=discussions, section_slug=section_slug, theme_slug=current_theme.slug, title='Topics list')
 
 @app.route('/forum/<section_slug>/<theme_slug>/<discussion_id>')
 def discussion(section_slug, theme_slug, discussion_id):
@@ -40,4 +40,4 @@ def discussion(section_slug, theme_slug, discussion_id):
     current_theme = Theme.query.filter(Theme.slug==theme_slug, Theme.section_id==current_section.id).first_or_404()
     current_discussion = Discussion.query.filter(Discussion.theme_id==current_theme.id, Discussion.id==discussion_id).first_or_404()
 
-    return render_template('discussion.html', discussion=current_discussion)
+    return render_template('discussion.html', discussion=current_discussion, title=current_discussion.theme)
