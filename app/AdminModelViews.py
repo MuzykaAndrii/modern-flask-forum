6863718284e188cb463e6a8628f328c1d665610f
MuzykaAndrii <andrymyzik@gmail.com>
2021-05-14 05:52:25 +0300

    upgraded model views in admin panel for section, theme, discussion and tag models

--- app/AdminModelViews.py
+++ app/AdminModelViews.py
@@ -1,19 +1,40 @@
 from flask_admin import AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
+from slugify import slugify
 
 class AdminAccess(ModelView):
     def is_accessible(self):
         return current_user.has_role('admin')
 
 class SectionModelView(AdminAccess):
-    pass
+    form_edit_rules  = ['name', 'description']
+    form_create_rules = ['name', 'description']
+
+    def on_model_change(self, form, model, is_created):
+        model.slug = slugify(form.name.data)
+        return super(SectionModelView, self).on_model_change(form, model, is_created)
 
 class ThemeModelView(AdminAccess):
-    pass
+    form_edit_rules = ['name']
+    form_create_rules = ['parent_section', 'name']
+
+    def on_model_change(self, form, model, is_created):
+        model.slug = slugify(form.name.data)
+        return super(ThemeModelView, self).on_model_change(form, model, is_created)
 
 class DiscussionModelView(AdminAccess):
-    pass
+    form_create_rules = ['parent_theme', 'theme', 'text', 'tags', 'creator']
+    form_edit_rules = ['parent_theme', 'theme', 'text', 'tags', 'creator']
+
+    def on_model_change(self, form, model, is_created):
+        model.slug = slugify(form.name.data)
+        return super(DiscussionModelView, self).on_model_change(form, model, is_created)
 
 class TagModelView(AdminAccess):
-    pass
+    form_create_rules = ['parent_section', 'name']
+    form_edit_rules = ['parent_section', 'name']
+
+    def on_model_change(self, form, model, is_created):
+        model.slug = slugify(form.name.data)
+        return super(TagModelView, self).on_model_change(form, model, is_created)
